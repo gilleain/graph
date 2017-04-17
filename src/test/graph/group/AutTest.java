ba07143442640728867e5886d00bf1ed97cb6287
maclean <gilleain.torrance@gmail.com>
2017-04-17 22:17:20 +0100

    Three-grid graph test

--- src/test/graph/group/AutTest.java
+++ src/test/graph/group/AutTest.java
@@ -2,6 +2,7 @@ package test.graph.group;
 
 import graph.group.GraphDiscretePartitionRefiner;
 import graph.model.IntGraph;
+import group.Partition;
 import group.Permutation;
 import group.PermutationGroup;
 
@@ -9,6 +10,23 @@ import org.junit.Test;
 
 
 public class AutTest {
+    
+    @Test
+    public void threeThreeSquare() {
+        IntGraph graph = new IntGraph("0:1,0:3,1:2,1:4,2:5,3:4,3:6,4:5,4:7,5:8,6:7,7:8");
+        Partition initial = Partition.fromString("[0|2,6,8|5,7|1,3|4]");
+//        Partition initial = Partition.fromString("0,2,6,8|1,3,5,7|4");
+//        Partition initial = Partition.fromString("4|1,3,5,7|0,2,6,8");
+//        Partition initial = Partition.fromString("4|1|3,5,7|0,2,6,8");
+//        Partition initial = Partition.unit(9);
+        GraphDiscretePartitionRefiner refiner = new GraphDiscretePartitionRefiner();
+        PermutationGroup group = refiner.getAutomorphismGroup(graph, initial);
+        int i = 0;
+        for (Permutation p : group.all()) {
+            System.out.println(i + "\t" + p.toCycleString());
+            i++;
+        }
+    }
 	
 	@Test
 	public void nautyBridgedSquare() {
